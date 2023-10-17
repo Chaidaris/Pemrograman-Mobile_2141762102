62b80709f5ad4b36091a3166af4c4a79ad18dccb
Chaidaris <2131710102@student.polinema.ac.id>
2023-10-17 08:30:29 +0700

    update

---
+++ lib/pertemuan8/README.md
@@ -0,0 +1,3 @@
+# pertemuan8
+
+A new Flutter project.
---
+++ lib/pertemuan8/android/app/src/main/kotlin/com/example/pertemuan8/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.pertemuan8.firebase
+
+import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+class MainActivity: FlutterActivity() {
+}
